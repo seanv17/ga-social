d6678cff40ffbaa4f945acaf023bdce8cea963e0
Sam <samuel1008@gmail.com>
2016-05-12 15:43:47 -0700

    added columns to users and migrated data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512211414) do
+ActiveRecord::Schema.define(version: 20160512224030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(version: 20160512211414) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "image"
+    t.string   "current_city"
+    t.string   "current_position"
+    t.boolean  "seeking_work"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
