8b455985a98b66f6b1f85ed9cf325d10276de791
Sam <samuel1008@gmail.com>
2016-05-12 15:52:52 -0700

    added current_employer and tech_preference to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512224030) do
+ActiveRecord::Schema.define(version: 20160512225042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 20160512224030) do
     t.string   "current_city"
     t.string   "current_position"
     t.boolean  "seeking_work"
+    t.string   "current_employer"
+    t.string   "tech_preference"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
