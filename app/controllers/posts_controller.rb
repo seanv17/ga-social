113cae3a71900a204f7814d5f946bf0971f4e282
Hila Vasiler <hila.marmel@gmail.com>
2016-05-13 08:44:56 -0700

    added new and create post methods and routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,26 @@ class PostsController < ApplicationController
     render :index
   end
 
+  def new
+    if current_user
+      @post = Post.new
+      @user = current_user
+      render :new
+    else
+      redirect_to splash_path
+    end
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.user = current_user
+    if post.save
+      redirect_to posts_path
+    else
+      redirect_to new_post_path
+    end
+  end
+
   def show
 
   end
@@ -18,10 +38,6 @@ class PostsController < ApplicationController
   end
 
   def add
-
-  end
-
-  def create
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
 
   root 'site#splash', as: :splash
 #-----------------------------USER/POST PATHS-----------------------------------
-  get "/users/:user_id/posts", to: "posts#index", as: :user_posts
-  post "/users/:user_id/posts", to: "posts#create"
-  get "/users/:user_id/posts/new", to: "posts#new", as: :new_user_post
-  get "/users/:user_id/posts/:id/edit", to: "posts#edit", as: :edit_user_post
-  get "/users/:user_id/posts/:id", to: "posts#show", as: :user_post
-  put "/users/:user_id/posts/:id", to: "posts#update"
-  delete "/users/:user_id/posts/:id", to: "posts#destroy"
+  # get "/users/:user_id/posts", to: "posts#index", as: :user_posts
+  # post "/users/:user_id/posts", to: "posts#create"
+  # get "/users/:user_id/posts/new", to: "posts#new", as: :new_user_post
+  # get "/users/:user_id/posts/:id/edit", to: "posts#edit", as: :edit_user_post
+  # get "/users/:user_id/posts/:id", to: "posts#show", as: :user_post
+  # put "/users/:user_id/posts/:id", to: "posts#update"
+  # delete "/users/:user_id/posts/:id", to: "posts#destroy"
 #-----------------------------USER PATHS----------------------------------------
   get "/users", to: "users#index", as: :users
   post "/users", to: "users#create"
@@ -20,6 +20,9 @@ Rails.application.routes.draw do
   put "/users/:id", to: "users#update"
   delete "/users/:id", to: "users#destroy"
 #-----------------------------POST PATHS----------------------------------------
-  get "posts", to:"posts#index", as: :posts 
+  get "/posts", to: "posts#index", as: :posts
+  post "/posts", to: "posts#create"
+  get "/posts/new", to: "posts#new", as: :new_post
+
 
 end
