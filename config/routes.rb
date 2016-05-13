66b71bd0e3f4db54a30eec1a384768da9e63cad6
Hila Vasiler <hila.marmel@gmail.com>
2016-05-12 20:59:25 -0700

    added post model, posts index method and routes for users and users/posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,24 @@
 Rails.application.routes.draw do
 
-
   devise_for :users
-  root 'site#splash', as: :splash
 
-  resources :users
+  root 'site#splash', as: :splash
+#-----------------------------USER/POST PATHS-----------------------------------
+  get "/users/:user_id/posts", to: "posts#index", as: :user_posts
+  post "/users/:user_id/posts", to: "posts#create"
+  get "/users/:user_id/posts/new", to: "posts#new", as: :new_user_post
+  get "/users/:user_id/posts/:id/edit", to: "posts#edit", as: :edit_user_post
+  get "/users/:user_id/posts/:id", to: "posts#show", as: :user_post
+  put "/users/:user_id/posts/:id", to: "posts#update"
+  delete "/users/:user_id/posts/:id", to: "posts#destroy"
+#-----------------------------USER PATHS---------------------------------------
+  get "/users", to: "users#index", as: :users
+  post "/users", to: "users#create"
+  get "/users/new", to: "users#new", as: :new_user
+  get "/users/:id/edit", to: "users#edit", as: :edit_user
+  get "/users/:id", to: "users#show", as: :user
+  put "/users/:id", to: "users#update"
+  delete "/users/:id", to: "users#destroy"
+
 
 end
