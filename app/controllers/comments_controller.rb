2c302cb0c9fb67c6afd62afd6db4f2f0959582ba
Bkliger <bkliger@comcast.net>
2016-05-14 16:22:13 -0700

    create comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,7 +9,10 @@ class CommentsController < ApplicationController
   end
 
   def create
+
     @comment = Comment.new(comment_params)
+    @comment.user = current_user
+    @comment.post_id = params[:id]
 
     if @comment.save
       flash[:success] = 'Your comment was successfully added!'
@@ -23,7 +26,7 @@ class CommentsController < ApplicationController
 private
 
   def comment_params
-    params.require(:comment).permit(:comment_body,:user_id,:post_id)
+    params.require(:comment).permit(:body,:user_id,:post_id)
   end
 
 
