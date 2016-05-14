00f2ac78d63861c6f4e090493e8ad5b2a91fb6d9
Sam <samuel1008@gmail.com>
2016-05-14 15:27:59 -0700

    updated post scss so we can see post, added paperclip to post, updated post controller, post new, and post index

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
+
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
